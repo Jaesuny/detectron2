46ee8d80d696073bf5dce27db215064c3099c034
Jaesun Park <diligensloth@gmail.com>
2020-10-29 18:08:04 +0900

    Add DETR

--- detectron2/modeling/meta_arch/__init__.py
+++ detectron2/modeling/meta_arch/__init__.py
@@ -9,6 +9,7 @@ from .panoptic_fpn import PanopticFPN
 from .rcnn import GeneralizedRCNN, ProposalNetwork
 from .retinanet import RetinaNet
 from .semantic_seg import SEM_SEG_HEADS_REGISTRY, SemanticSegmentor, build_sem_seg_head
+from .detectron_detr import Detr
 
 
 __all__ = list(globals().keys())
